5bf58ed32e012623beb98214efbbe4e0e7e6fd0f
wcfx <wcfx.dev@gmail.com>
2022-09-07 18:29:27 -0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Wagner Barboza Goulart on 07/09/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
